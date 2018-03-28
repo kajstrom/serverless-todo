2beeb3c16841a4bf905f8fcbc0ac2f16ce2d2ee6
Kaj Ström <kaj.strom@kapsi.fi>
2018-03-28 19:26:16 +0300

    Right configuration for DynamoDb added to creation scripts

--- scripts/create-table-todo.bat
+++ scripts/create-table-todo.bat
@@ -1,1 +1,2 @@
 aws dynamodb create-table --table-name todos --attribute-definitions AttributeName=todoId,AttributeType=S --key-schema AttributeName=todoId,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --region eu-central-1 --query TableDescription.TableArn --output text
+aws iam put-role-policy --role-name serverless-todo-executor --policy-name TodoDynamoDb --policy-document file://./dynamodb.json
